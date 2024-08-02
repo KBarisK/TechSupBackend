2fa2615190891534c206e780d03db6dae8811b2d
Barış Kılıç <bariskilic2003@gmail.com>
2024-08-02 09:37:44 +0300

    initiliaze project
    initiliaze project

---
+++ src/main/java/com/backend/techsupbackend/TechSupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.techsupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechSupBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/techsupbackend/TechSupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.techsupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSupBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
