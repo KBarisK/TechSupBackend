17ce640c5f03eb262c2cfffe1044edeff84d41ee
Barış Kılıç <bariskilic2003@gmail.com>
2024-08-14 11:17:00 +0300

    updated delete methods

--- src/main/java/com/backend/techsupbackend/service/DepartmentService.java
+++ src/main/java/com/backend/techsupbackend/service/DepartmentService.java
@@ -2,7 +2,9 @@ package com.backend.techsupbackend.service;
 
 import com.backend.techsupbackend.model.Department;
 import com.backend.techsupbackend.model.Directorate;
+import com.backend.techsupbackend.model.TechFile;
 import com.backend.techsupbackend.repository.DepartmentRepository;
+import com.backend.techsupbackend.repository.TechFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +15,36 @@ public class DepartmentService {
     @Autowired
     DepartmentRepository departmentRepository;
 
+    @Autowired
+    TechFileRepository techFileRepository;
+
     public void addDepartment(Department department) {
         departmentRepository.save(department);
     }
 
     public void deleteDepartment(String name) {
+        String[] strings = name.split("%20");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < strings.length - 1; i++) {
+            stringBuilder.append(strings[i]).append(" ");
+        }
+        stringBuilder.append(strings[strings.length - 1]);
+        name = stringBuilder.toString();
         Department department = departmentRepository.findByName(name);
         departmentRepository.delete(department);
+        List<TechFile> fileList = techFileRepository.findAll();
+        for (int i = 0; i < fileList.size(); i++) {
+            TechFile file = fileList.get(i);
+            List<String> departmentList = file.getDepartments();
+            for (int j = 0; j < departmentList.size() ; j++) {
+                String department_name = departmentList.get(i);
+                if (department_name.equals(name)){
+                    departmentList.remove(i);
+                }
+            }
+            file.setDirectorateList(departmentList);
+            techFileRepository.save(file);
+        }
     }
 
     public List<Department> getAll() {
--- src/main/java/com/backend/techsupbackend/service/DirectorateService.java
+++ src/main/java/com/backend/techsupbackend/service/DirectorateService.java
@@ -1,7 +1,9 @@
 package com.backend.techsupbackend.service;
 
 import com.backend.techsupbackend.model.Directorate;
+import com.backend.techsupbackend.model.TechFile;
 import com.backend.techsupbackend.repository.DirectorateRepository;
+import com.backend.techsupbackend.repository.TechFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +15,37 @@ public class DirectorateService {
     @Autowired
     DirectorateRepository directorateRepository;
 
+    @Autowired
+    TechFileRepository techFileRepository;
+
     public void addDirectorate(Directorate directorate){
         directorateRepository.save(directorate);
     }
 
     public void deleteDirectorate(String name){
+        String[] strings = name.split("%20");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < strings.length - 1; i++) {
+            stringBuilder.append(strings[i]).append(" ");
+        }
+        stringBuilder.append(strings[strings.length - 1]);
+        name = stringBuilder.toString();
         Directorate directorate =directorateRepository.findByName(name);
         directorateRepository.delete(directorate);
+        List<TechFile> fileList = techFileRepository.findAll();
+        for (int i = 0; i < fileList.size(); i++) {
+            TechFile file = fileList.get(i);
+            List<String> directorateList = file.getDirectorateList();
+            System.out.println(directorateList);
+            for (int j = 0; j < directorateList.size() ; j++) {
+                String directorate_name = directorateList.get(i);
+                if (directorate_name.equals(name)){
+                    directorateList.remove(i);
+                }
+            }
+            file.setDirectorateList(directorateList);
+            techFileRepository.save(file);
+        }
     }
 
     public List<Directorate> getAll(){
