9defd989fb4295d21d6ffbd6b803564a2714ab5c
Barış Kılıç <bariskilic2003@gmail.com>
2024-08-26 21:38:39 +0300

    jwt check for admin

--- src/main/java/com/backend/techsupbackend/controller/AuthController.java
+++ src/main/java/com/backend/techsupbackend/controller/AuthController.java
@@ -20,7 +20,7 @@ public class AuthController {
         this.authService = authService;
     }
 
-    @PostMapping("/register")
+    @PostMapping("/admin/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody Admin admin){
         return ResponseEntity.ok(authService.register(admin));
     }
